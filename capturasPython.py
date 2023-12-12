6fe7e85c817182942c2358a9d96704bd03e22639
Simone-Lopes <simone.santos@sptech.school>
2023-12-11 22:55:15 -0300

    Correção de arquivos

--- capturasPython.py
+++ capturasPython.py
@@ -2,17 +2,17 @@ import mysql.connector
 import psutil
 import time
 from datetime import datetime
-import platform 
+import platform
+import pymssql
 
-#Meu banco local
-connection = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='APUXxx2707',
-    database='test'
+# Configurações SQL Server
+sql_server_cnx = pymssql.connect(
+    server='52.45.220.247',
+    database='test',
+    user='sa',
+    password='sptech'
 )
-
-cursor = connection.cursor()
+cursor_sql_server = sql_server_cnx.cursor()
 
 # Criando a estrutura de repetição para que os valores dos componentes se atualizam
 while True:
@@ -25,35 +25,41 @@ while True:
     dia = datetime.now()
 
     # SQL para inserir na tabela RegistrosTRUSTED (CPU)
-   
-    sql7 = "INSERT INTO RegistrosTRUSTED (textosCapturados, dataHora, fkComponente, fkIdservidor) VALUES (%s, %s, %s, %s)"
-    values7 = (nome_processador, dia.strftime('%Y-%m-%d %H:%M:%S'), 7, 1)
 
-    sql8 = "INSERT INTO RegistrosTRUSTED (textosCapturados, dataHora, fkComponente, fkIdservidor) VALUES (%s, %s, %s, %s)"
-    values8 = (arquitetura_processador, dia.strftime('%Y-%m-%d %H:%M:%S'), 8, 1)
+    # SQL para inserir na tabela RegistrosTRUSTED do MySQL (CPU)
+    sql8_sql = "INSERT INTO RegistrosTRUSTED (dataHora, fkComponente, fkIdServidor, textoCapturado) VALUES (%s, %s, %s, %s)"
+    values8_sql = (dia.strftime('%Y-%m-%d %H:%M:%S'), 8, 5, nome_processador)
 
-    sql9 = "INSERT INTO RegistrosTRUSTED (dadosCapturados, dataHora, fkComponente, fkIdservidor) VALUES (%s, %s, %s, %s)"
-    values9 = (nucleos_fisicos, dia.strftime('%Y-%m-%d %H:%M:%S'), 9, 1)
+    sql9_sql = "INSERT INTO RegistrosTRUSTED (dataHora, fkComponente, fkIdServidor, textoCapturado) VALUES (%s, %s, %s, %s)"
+    values9_sql = (dia.strftime('%Y-%m-%d %H:%M:%S'), 9, 5, arquitetura_processador)
 
-    sql10 = "INSERT INTO RegistrosTRUSTED (dadosCapturados, dataHora, fkComponente, fkIdservidor) VALUES (%s, %s, %s, %s)"
-    values10 = (nucleos_logicos, dia.strftime('%Y-%m-%d %H:%M:%S'), 10, 1)
+    sql10_sql = "INSERT INTO RegistrosTRUSTED (dadosCapturados, dataHora, fkComponente, fkIdServidor) VALUES (%s, %s, %s, %s)"
+    values10_sql = (nucleos_fisicos, dia.strftime('%Y-%m-%d %H:%M:%S'), 10, 5)
 
-    sql11 = "INSERT INTO RegistrosTRUSTED (dadosCapturados, dataHora, fkComponente, fkIdservidor) VALUES (%s, %s, %s, %s)"
-    values11 = (frequencia_hz, dia.strftime('%Y-%m-%d %H:%M:%S'), 11, 1)
+    sql11_sql = "INSERT INTO RegistrosTRUSTED (dadosCapturados, dataHora, fkComponente, fkIdServidor) VALUES (%s, %s, %s, %s)"
+    values11_sql = (nucleos_logicos, dia.strftime('%Y-%m-%d %H:%M:%S'), 11, 5)
+
+    sql12_sql = "INSERT INTO RegistrosTRUSTED (dadosCapturados, dataHora, fkComponente, fkIdServidor) VALUES (%s, %s, %s, %s)"
+    values12_sql = (frequencia_hz, dia.strftime('%Y-%m-%d %H:%M:%S'), 12, 5)
+
 
     try:
         # Executa a inserção
-        cursor.execute(sql7, values7)
-        cursor.execute(sql8, values8)
-        cursor.execute(sql9, values9)
-        cursor.execute(sql10, values10)
-        cursor.execute(sql11, values11)
+        cursor_sql_server.execute(sql8_sql, values8_sql)
+        cursor_sql_server.execute(sql9_sql, values9_sql)
+        cursor_sql_server.execute(sql10_sql, values10_sql)
+        cursor_sql_server.execute(sql11_sql, values11_sql)
+        cursor_sql_server.execute(sql12_sql, values12_sql)
 
         # Confirma as alterações no banco de dados
-        connection.commit()
+        sql_server_cnx.commit()
         print("Inserção de dados realizada com sucesso!")
 
-    except mysql.connector.Error as err:
-        print("Erro ao inserir nas tabelas Registros:", err)
+    except pymssql.OperationalError as op_err:
+        print("Erro operacional ao inserir nas tabelas:", op_err)
+    except pymssql.ProgrammingError as prog_err:
+        print("Erro de programação ao inserir nas tabelas:", prog_err)
+    except Exception as e:
+        print("Erro inesperado ao inserir nas tabelas:", e)
 
     time.sleep(1)
